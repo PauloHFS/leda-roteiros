11af97bc0d7a40742f705ea3c61a5b1a872f20a7
Paulo Hernane <paulohernane10@gmail.com>
2021-03-15 14:26:34 -0300

    insertionsort terminando

--- R01-02-Rot-SimpleSorting-Bidirectional-Bubble/src/main/java/sorting/simpleSorting/InsertionSort.java
+++ R01-02-Rot-SimpleSorting-Bidirectional-Bubble/src/main/java/sorting/simpleSorting/InsertionSort.java
@@ -1,6 +1,7 @@
 package sorting.simpleSorting;
 
 import sorting.AbstractSorting;
+import util.Util;
 
 /**
  * As the insertion sort algorithm iterates over the array, it makes the
@@ -12,7 +13,17 @@ public class InsertionSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not Implemented yet!");
+		if (leftIndex >= 0 && rightIndex <= array.length && rightIndex - leftIndex > 0) {
+			for (int i = leftIndex + 1; i <= rightIndex; i++) {
+
+				int j = i;
+
+				while (j > 0 && array[j].compareTo(array[j-1]) < 0) {
+					Util.swap(array, j, j-1);
+					j -= 1;
+				}
+
+			}
+		}
 	}
 }
--- R01-02-Rot-SimpleSorting-Bidirectional-Bubble/src/test/java/sorting/test/StudentSortingTest.java
+++ R01-02-Rot-SimpleSorting-Bidirectional-Bubble/src/test/java/sorting/test/StudentSortingTest.java
@@ -40,7 +40,7 @@ public class StudentSortingTest {
 	private void getImplementation() {
 		// TODO O aluno deve instanciar sua implementação abaixo ao invés de
 		// null
-		this.implementation = new SelectionSort<Integer>();
+		this.implementation = new InsertionSort<Integer>();
 		//Assert.fail("Implementation not provided");
 	}
 
