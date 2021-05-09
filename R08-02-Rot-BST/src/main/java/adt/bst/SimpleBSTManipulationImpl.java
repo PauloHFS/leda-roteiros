d4b47995b1022f52d63d0f7b8a35abc78993c512
Paulo Hernane <paulo.hernane.silva@ccc.ufcg.edu.br>
2021-05-09 14:56:53 -0300

    Floor and ceil done

--- R08-02-Rot-BST/src/main/java/adt/bst/SimpleBSTManipulationImpl.java
+++ R08-02-Rot-BST/src/main/java/adt/bst/SimpleBSTManipulationImpl.java
@@ -10,8 +10,27 @@ public class SimpleBSTManipulationImpl<T extends Comparable<T>> implements Simpl
 
 	@Override
 	public boolean equals(BST<T> tree1, BST<T> tree2) {
-		// TODO Implement this method
-		throw new UnsupportedOperationException("Not implemented yet!");
+		return this.equals((BSTNode<T>) tree1.getRoot(), (BSTNode<T>) tree2.getRoot());
+	}
+
+	private boolean equals(BSTNode<T> node1, BSTNode<T> node2) {
+		boolean isEqual = false;
+
+		if (node1.isEmpty() && node2.isEmpty()) {
+			isEqual = true;
+		} else if (!node1.isEmpty() && !node2.isEmpty()) {
+			
+		}
+
+		if () {
+			if (node1.getData().compareTo(node2.getData()) == 0) {
+
+			}
+		} else if (node1.isEmpty() && node2.isEmpty()) {
+			isEqual = true;
+		}
+
+		return isEqual;
 	}
 
 	@Override
--- R08-02-Rot-BST/src/main/java/adt/bst/extended/FloorCeilBSTImpl.java
+++ R08-02-Rot-BST/src/main/java/adt/bst/extended/FloorCeilBSTImpl.java
@@ -1,6 +1,7 @@
 package adt.bst.extended;
 
 import adt.bst.BSTImpl;
+import adt.bst.BSTNode;
 
 /**
  * Note que esta classe estende sua implementacao de BST (BSTImpl).
@@ -11,14 +12,81 @@ public class FloorCeilBSTImpl extends BSTImpl<Integer> implements FloorCeilBST {
 
 	@Override
 	public Integer floor(Integer[] array, double numero) {
-		//TODO Implemente seu codigo aqui
-		throw new UnsupportedOperationException("Not implemented yet!");
+		for (Integer num : array) {
+			this.insert(num);
+		}
+
+		return this.floor(this.getRoot(), numero);
+	}
+
+	private Integer floor(BSTNode<Integer> node, double numero) {
+
+		Integer floor = null;
+
+		if (!node.isEmpty()) {
+
+			if ((double) node.getData() == numero) {
+
+				floor = node.getData();
+
+			} else if (node.getData() > numero) {
+
+				floor = this.floor((BSTNode<Integer>) node.getLeft(), numero);
+
+			} else {
+
+				floor = this.floor((BSTNode<Integer>) node.getRight(), numero);
+
+				if (floor == null) {
+
+					floor = node.getData();
+
+				}
+
+			}
+
+		}
+
+		return floor;
 	}
 
 	@Override
 	public Integer ceil(Integer[] array, double numero) {
-		//TODO Implemente seu codigo aqui
-		throw new UnsupportedOperationException("Not implemented yet!");
+		for (Integer num : array) {
+			this.insert(num);
+		}
+
+		return this.ceil(this.getRoot(), numero);}
+
+	private Integer ceil(BSTNode<Integer> node, double numero) {
+
+		Integer ceil = null;
+
+		if (!node.isEmpty()) {
+
+			if ((double) node.getData() == numero) {
+
+				ceil = node.getData();
+
+			} else if (node.getData() < numero) {
+
+				ceil = this.ceil((BSTNode<Integer>) node.getRight(), numero);
+
+			} else {
+
+				ceil = this.ceil((BSTNode<Integer>) node.getLeft(), numero);
+
+				if (ceil == null) {
+
+					ceil = node.getData();
+
+				}
+
+			}
+
+		}
+
+		return ceil;
 	}
 
 }
